77a39aa1f7f4b2a8e058220e8985b5e451f8b41a
littleflute <13699175164@sina.cn>
2017-07-19 16:22:30 -0500

    Add files via upload

---
+++ Odetta/American Folk Pioneer/m.js
@@ -0,0 +1,7 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Black Woman.mp3";		
+f[2]	= "02 The Fox.mp3";		
+f[3]	= "03 Ancestors- 900 Miles-Red Clay Country-Another Man Done Gone-No.mp3";		
+f[4]	= "04 Ol' Lady Sally.mp3";		
+f[5]	= "05 Amazing Grace.mp3";		
---
+++ Odetta/Vanguard Visionaries/m.js
@@ -0,0 +1,12 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Midnight Special.mp3";		
+f[2]	= "02 He's Got the Whole World in His Hands.mp3";		
+f[3]	= "03 Cotton Fields.mp3";		
+f[4]	= "04 House of the Rising Sun.mp3";		
+f[5]	= "05 Another Man Done Gone.mp3";		
+f[6]	= "06 Livin' with the Blues.mp3";		
+f[7]	= "07 Special Delivery Blues.mp3";		
+f[8]	= "08 Down on Me.mp3";		
+f[9]	= "09 Cool Water.mp3";		
+f[10]	= "10 Jumpin' Judy.mp3";		
